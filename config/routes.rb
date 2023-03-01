aa326e7d723f2ac96d3edaa6dea05becc0b0a346
daiki2828 <114804118+daiki2828@users.noreply.github.com>
2023-03-01 08:04:21 +0000

    home作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins, controllers:{
+  devise_for :admin, controllers:{
     sessions:"admin/sessions"
   }
   devise_for :users, controllers:{
     registrations:"public/registrations",
     sessions:"public/sessions"
   }
+
+  scope module: :public do
+    root :to => "homes#top"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
